ae5fe00f45d6ed72e967c76622378a8105f080e7
Mushie <77275833+Mushie1@users.noreply.github.com>
2023-06-20 15:11:08 +1000

    Update settings.js

---
+++ scripts/settings.js
@@ -0,0 +1,18 @@
+﻿Hooks.on('init', () => {
+  game.settings.register('wylies-file-browser-resizer', 'maxHeight', {
+    name: 'Max Height',
+    hint: 'Set the maximum height of the filepicker directory in pixels.',
+    scope: 'client',
+    config: true,
+    default: 200,
+    type: Number,
+    onChange: value => {
+      document.documentElement.style.setProperty('--wfb-resizer-max-height', `${value}px`);
+    }
+  });
+});
+
+Hooks.on('ready', () => {
+  const maxHeight = game.settings.get('wylies-file-browser-resizer', 'maxHeight');
+  document.documentElement.style.setProperty('--wfb-resizer-max-height', `${maxHeight}px`);
+});
